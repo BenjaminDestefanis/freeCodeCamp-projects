b0553c51c8040b064747659e8cc952141ce56cca
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-07-19 17:16:17 -0300

    7/19/24 5:16PM

---
+++ D3/Visualidador-de-datos/index.js
@@ -0,0 +1,9 @@
+
+
+let marginTop    = 10
+let marginBottom = 25
+let marginLeft   = 60
+let marginRight  = 15
+let width        = 700 - marginLeft
+let height       = 400 - marginBottom
+let maxDate      = new Date(data)
